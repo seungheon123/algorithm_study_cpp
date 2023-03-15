582f8513cdfd161a28794d8da04a351de653611d
한승헌 <seungheon123@khu.ac.kr>
2023-03-15 10:44:15 +0900

    백준 1152

---
+++ algorithm/1152.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	int cnt = 1;
+	string str;
+
+	getline(cin, str);
+	int len = str.length();
+
+	for (int i = 0; i < len; i++) {
+
+		if (str[i] == ' ') {
+			cnt++;
+		}
+	}
+
+	if (str[0] == ' ') cnt--;
+	if (str[len - 1] == ' ') cnt--;
+
+	cout << cnt;
+}
